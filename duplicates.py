ce719805f8ef3ae5408e643a8be4a83c139907ff
obook <obooklage@free.fr>
2025-03-24 15:44:15 +0100

    Renamed and remove duplicates.

--- duplicates.py
+++ duplicates.py
@@ -8,6 +8,7 @@ Created on Mon Mar 24 13:15:46 2025
 import os
 import glob
 import hashlib
+from pathlib import Path, PurePath
 
 
 def md5(fname):
@@ -21,13 +22,32 @@ files = []
 
 for file in sorted(
     glob.glob(
-        os.path.join(path, "**", "*.mid"),
-        recursive=True,
-    )
-):
-
+        os.path.join(path, "**", "*.mid"), recursive=True)
+   ):
     files.append([file, md5(file)])
 
-for item in files:
-    file, md5 = item
-    print(file, md5)
+with open("duplicate.txt", "w") as f:
+    counter = 0
+    for index1 in range(len(files)-1):
+        for index2 in range(index1, len(files)):
+            if files[index1][1] == files[index2][1] and files[index1][0] != files[index2][0]:
+                counter += 1
+                s = f"Duplicate #{counter} {files[index1][0]} ~ {files[index2][0]}\n"
+                print(s)
+                f.write(s)
+                '''
+                BOB MACE :
+                renomer files[index1][0] en files[index2][0]
+                ./MACE BOB/WORLDNED.mid
+                ./PIANO BAR/What the World Needs Now.mid
+                
+                on récupère What the World Needs Now.mid
+                '''
+                # os.remove(files[index2][0])
+                # os.remove(files[index2][0])
+                # path = pathlib.PurePath(file)
+                # p = Path(files[index2][0])
+                # print(PurePath(files[index2][0]))
+                
+# Ca déplace ? OUI en racine !
+# os.rename("./MACE BOB/ALLTHING.mid", os.path.join("./MACE BOB","All Things You Are.mid"))
